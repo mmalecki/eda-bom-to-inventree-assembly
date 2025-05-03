a0d62c0023f21da5cb75fdfc18cb62b8948c13a2
Maciej Małecki <me@mmalecki.com>
2025-05-03 02:59:59 +0200

    Emit the PCB part
    Fixes #1.
    
    Minor clean-up.

--- eda-bom-to-inventree-assembly.mjs
+++ eda-bom-to-inventree-assembly.mjs
@@ -14,33 +14,50 @@ const argv = yargs(hideBin(process.argv))
   .option('pcb-part-ipn', { type: 'string', description: 'Include PCB with this part IPN in the BOM' })
   .parse()
 
-
 const MPN_FIELDS = ['MPN', 'Part Number', 'Manufacturer Part Number', 'Part ID'];
 const IPN_FIELD = 'IPN'
 
+let headerRow = stringifySync([Object.keys(inventreeRecord({}))])
+
+function inventreeRecord({ partName, partIpn, quantity, reference, note = '', optional = false, consumable = false, inherited = true }) {
+  return {
+    'Part_Name': partName,
+    'Part_IPN': partIpn,
+    'Quantity': quantity,
+    'Reference': reference,
+    'Note': note,
+    'Optional': optional.toString(),
+    'Consumable': consumable.toString(),
+    'Inherited': inherited.toString(),
+  }
+}
 
 let pcbRow;
 
 if (argv['pcb-part-name'] || argv['pcb-part-ipn']) {
-  pcbRow = [stringifySync(
-     [{
-      'Part_Name': argv['pcb-part-name'] || '',
-      'Part_IPN':  argv['pcb-part-ipn'] || '',
-      'Quantity': 1,
-      'Reference': 'PCB',
-      'Note': '',
-      'Optional': 'false',
-      'Consumable': 'false',
-      'Inherited': 'true',
-    }]
-  )]
+  pcbRow = stringifySync(
+     [inventreeRecord({
+       partName: argv['pcb-part-name'] || '',
+       partIpn:  argv['pcb-part-ipn'] || '',
+       quantity: 1,
+       reference: 'PCB',
+     })]
+  )
 }
 
 const in_ = process.stdin
 const out = process.stdout
 
+// Write the header ourselves, so that we can insert the PCB row right
+// afterwards, but before piping, where we lose control of the stream.
+// I'm sure there's a "clever" way to do that, but that's what I'm
+// trying to avoid here.
+out.write(headerRow)
+
+if (pcbRow) out.write(pcbRow)
+
 in_
-  .pipe(parse({ columns: true, delimeter: ',' }))
+  .pipe(parse({ columns: true }))
   .pipe(transform((row) => {
     const ref = row['Reference'];
     const quantity = row['Qty'] || row['Quantity'] || '1';
@@ -50,16 +67,12 @@ in_
       return acc || row[field] || '';
     }, '');
 
-    return {
-      'Part_Name': partMpn,
-      'Part_IPN': row[IPN_FIELD] || '',
-      'Quantity': quantity,
-      'Reference': ref,
-      'Note': '',
-      'Optional': 'false',
-      'Consumable': 'false',
-      'Inherited': 'true',
-    };
+    return inventreeRecord({
+      partName: partMpn,
+      partIpn: row[IPN_FIELD] || '',
+      quantity,
+      reference: ref,
+    });
   }))
-  .pipe(stringify({ header: true }))
+  .pipe(stringify({ header: false }))
   .pipe(out)
